bac22f508abdf897257c561058b50881ededacbb
Sander Smeenk <sander@bit.nl>
2022-01-23 21:03:20 +0100

    prevent duplicate perms, etc

--- wcb_bot/modules/chan-perm.py
+++ wcb_bot/modules/chan-perm.py
@@ -3,7 +3,7 @@ def config(wcb):
         'events': [],
         'commands': ['chan-perm', 'chan-perms'],
         'permissions': ['perm'],
-        'help': "usage is chan-perm <add|remove> <permission1> [..<permissionN>] - global perms are channel specific"
+        'help': "usage is chan-perm <list|add|remove> <permission1> [..permissionN] [channel]"
     }
 
 
@@ -15,35 +15,42 @@ def run(wcb, event):
     mode = 'list'
     if len(args_arr) > 0 and args_txt is not '':
         mode = args_arr.pop(0)
-        wcb.mlog("derp --%s--" % mode)
+
+    channel = wcb.event['channel']
+    if wcb.re.match('^[#&]', args_arr[-1]):
+        channel = args_arr.pop(-1)
+
     # args_arr should now only hold permissions to set
 
     if 'chan-perm' not in wcb.state:
         wcb.state['chan-perm'] = {}
 
-    if wcb.event['channel'] not in wcb.state['chan-perm']:
-        wcb.state['chan-perm'][wcb.event['channel']] = []
+    if channel not in wcb.state['chan-perm']:
+        wcb.state['chan-perm'][channel] = []
 
     if wcb.re.match('(?:list|show)', mode):
-        counter = len(wcb.state['chan-perm'][wcb.event['channel']])
+        counter = len(wcb.state['chan-perm'][channel])
         if not counter:
-            return wcb.say("No global permissions on '%s'" % wcb.event['channel'])
+            return wcb.say("No global permissions on '%s'" % channel)
         return wcb.say("There's %s global permissions on '%s': %s" %
-            (counter, wcb.event['channel'], sorted(wcb.state['chan-perm'][wcb.event['channel']])))
+            (counter, channel, sorted(wcb.state['chan-perm'][channel])))
 
     if wcb.re.match('(?:set|add)', mode):
         counter = 0
         for permission in args_arr:
-            wcb.state['chan-perm'][wcb.event['channel']].append(permission)
+            if permission in wcb.state['chan-perm'][channel]:
+                wcb.say("Permission '%s' is already set on '%s'" % (permission, channel))
+                continue
+            wcb.state['chan-perm'][channel].append(permission)
             counter += 1
         wcb.save_obj_as_json(wcb.state, wcb.state['bot_config'])
-        return wcb.say("Added %d global permissions to '%s'" % (counter, wcb.event['channel']))
+        return wcb.say("Added %d global permissions to '%s'" % (counter, channel))
 
     elif wcb.re.match('(?:rem(?:ove)?|del(?:ete)?)', mode):
         counter = 0
         for permission in args_arr:
-            if permission in wcb.state['chan-perm'][wcb.event['channel']]:
-                wcb.state['chan-perm'][wcb.event['channel']].remove(permission)
+            if permission in wcb.state['chan-perm'][channel]:
+                wcb.state['chan-perm'][channel].remove(permission)
                 counter += 1
         wcb.save_obj_as_json(wcb.state, wcb.state['bot_config'])
         return wcb.say("Removed %d permissions from '%s'" % (counter, event['channel']))
