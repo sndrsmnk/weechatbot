5e4b55787fa96a11b3ac019a10a030ac8e4a45ab
Peter van Dijk <peter@7bits.nl>
2023-09-13 23:02:41 +0200

    add v6 multicast, plus forgotten test line for v4 multicast

--- wcb_bot/modules/oui.py
+++ wcb_bot/modules/oui.py
@@ -39,17 +39,29 @@ def OUItrySpecial(mac, extra):
     except:
         return []
 
-    if len(bmac) == 6 and mac.startswith('00005E'): # IANA
-        # https://www.iana.org/assignments/ethernet-numbers/ethernet-numbers.xhtml#ethernet-numbers-1
-        if bmac[3] == 0:
-            if bmac[4] == 1:   # 00-00-5E-00-01-xx
-                extra.append('IPv4 VRRP')
-            elif bmac[4] == 2:
-                extra.append('IPv6 VRRP')
-            else:
-                return []
+    if len(bmac) == 6:
+        if mac.startswith('00005E'):  # IANA
+            # https://www.iana.org/assignments/ethernet-numbers/ethernet-numbers.xhtml#ethernet-numbers-1
+            if bmac[3] == 0:
+                if bmac[4] == 1:   # 00-00-5E-00-01-xx
+                    extra.append('IPv4 VRRP')
+                elif bmac[4] == 2:
+                    extra.append('IPv6 VRRP')
+                elif bmac[4] == 0x53:
+                    extra.append('Documentation range')
+                else:
+                    return
 
-            extra.append('id %d' % (bmac[5],))
+                extra.append('id %d' % (bmac[5],))
+        elif mac.startswith('01005E'):  # v4 multicast group
+            if bmac[3] <= 0x7f:
+                addr = '224.%d.%d.%d' % (bmac[3], bmac[4], bmac[5])
+                extra.append("IPv4 "+addr+"?")
+            elif bmac[3] <= 0x8f:
+                extra.append("MPLS [RFC5332]")
+        elif mac.startswith('3333'):  # v6 multicast
+            extra.append("IPv6 multicast for xx::%x%02x:%x%02x" % (bmac[2], bmac[3], bmac[4], bmac[5]))
+
 
 def macvendorsLookup(wcb, mac_input):
     res = requests.get("https://api.macvendors.com/" + mac_input)
@@ -170,6 +182,8 @@ def test_lookup():
             ('019069', ['Juniper Networks (multicast)']),
             ('00005E-00-02-01', ['Icann, Iana Department (IPv6 VRRP, id 1)', 'ICANN, IANA Department (IPv6 VRRP, id 1)']),
             ('0000:5E:00-01-55', ['Icann, Iana Department (IPv4 VRRP, id 85)', 'ICANN, IANA Department (IPv4 VRRP, id 85)']),
+            ('01005E-40-41-42', ['Icann, Iana Department (IPv4 224.64.65.66?, multicast)', 'ICANN, IANA Department (IPv4 224.64.65.66?, multicast)']),
+            ('333340414243', ['unknown (IPv6 multicast for xx::4041:4243, multicast, admindefined)']),
             ('3c:e1:a1:4c:dc:ac', ['Universal Global Scientific Industrial', 'Universal Global Scientific Industrial Co., Ltd.']),
             ('525400', ['unknown (qemu/kvm?, admindefined)', 'Not Found (qemu/kvm?, admindefined)']),
           ]:
