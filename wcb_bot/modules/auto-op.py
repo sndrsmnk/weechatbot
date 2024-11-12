6f3babf11b51d6463131cefc032c5f2e3e3ae4ff
Sander Smeenk <ssmeenk@freshdot.net>
2024-11-12 12:10:39 +0100

    Actually support (auto-)voice :P

--- wcb_bot/modules/auto-op.py
+++ wcb_bot/modules/auto-op.py
@@ -6,22 +6,29 @@ def config(wcb):
         'help': "Test"
     }
 
-
 def run(wcb, event):
     if not event['bot_is_op']:
         return wcb.mlog("Bot is not opped on channel '%s'. Can't auto-op/auto-voice." % event['channel'])
 
-    if not wcb.perms('owner') and wcb.perms('dno'):
+    if not wcb.perms('owner') and (wcb.perms('dno') or wcb.perms('dnv')):
         return
 
     tgtNick = event['nick']
     if event['command_args']:
+        cmd = event['command']
         tgtNick = event['command_args'].split(' ')[0]
+    else:
+        if wcb.perms('auto-op'):
+            cmd = 'op'
+        elif wcb.perms('auto-voice'):
+            cmd = 'voice'
 
-    log = "Gave ops to '%s' on channel '%s'" % (tgtNick, event['channel'])
+    log = f"Gave {cmd} to '{tgtNick}' on channel '{event['channel']}'"
     if event['command_args']:
-        log += " on behalf of '%s'" % event['nick']
+        log += f" on behalf of '{event['nick']}'"
     log += "."
     wcb.mlog(log)
-    wcb.weechat.command(event['weechat_buffer'], '/op ' + tgtNick)
+
+    wcb.weechat.command(event['weechat_buffer'], f"/{cmd} {tgtNick}")
     return wcb.weechat.WEECHAT_RC_OK
+
