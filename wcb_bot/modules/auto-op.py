c9a1aca27cb1aa1617247e8cd3992fc4473c08a6
Sander Smeenk <ssmeenk@freshdot.net>
2024-11-13 17:09:05 +0100

    check if tgtNick already has +o or +v before dealing +o or +v

--- wcb_bot/modules/auto-op.py
+++ wcb_bot/modules/auto-op.py
@@ -20,8 +20,23 @@ def run(wcb, event):
     else:
         if wcb.perms('auto-op'):
             cmd = 'op'
+            prefix = '@'
         elif wcb.perms('auto-voice'):
             cmd = 'voice'
+            prefix = '+'
+
+    # Figure out of nick already has ops/voice.
+    infolist = wcb.weechat.infolist_get('irc_nick', '', '%s,%s' % (event['server'], event['channel']))
+    while wcb.weechat.infolist_next(infolist):
+        nick = wcb.weechat.infolist_string(infolist, 'name')
+        if nick != tgtNick:
+            continue
+        if prefix in wcb.weechat.infolist_string(infolist, 'prefix'):
+            wcb.mlog(f"Nick '{tgtNick}' on channel '{event['channel']}' already has {cmd}")
+            wcb.weechat.infolist_free(infolist)
+            return wcb.weechat.WEECHAT_RC_OK
+
+    wcb.weechat.infolist_free(infolist)
 
     log = f"Gave {cmd} to '{tgtNick}' on channel '{event['channel']}'"
     if event['command_args']:
