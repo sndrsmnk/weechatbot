b953ebff07ec5cff5051a773715595fe642cb021
Sander Smeenk <ssmeenk@freshdot.net>
2024-11-14 17:14:01 +0100

    make auto-op group voice/op up to three nicks

--- wcb_bot/modules/auto-op.py
+++ wcb_bot/modules/auto-op.py
@@ -1,13 +1,50 @@
 def config(wcb):
     return {
-        'events': ['irc_in2_JOIN'],
+        'events': ['timer_signal', 'irc_in2_JOIN'],
         'commands': ['op', 'voice'],
         'permissions': ['auto-op', 'auto-voice'],
         'help': "Test"
     }
 
+
+def _find_buffer_and_do(wcb, servername, channel, cmd):
+    obuffer = False
+    servers = wcb.weechat.infolist_get("irc_channel", "", f"{servername},{channel}")
+    while wcb.weechat.infolist_next(servers):
+        obuffer = wcb.weechat.infolist_pointer(servers, 'buffer')
+    wcb.weechat.infolist_free(servers)
+    if not obuffer:
+        wcb.mlog(f"Auto-op could not find buffer for {servername}.{channel} '{cmd}'")
+        return wcb.weechat.WEECHAT_RC_OK
+    wcb.mlog(f"Auto-op ran {servername}.{channel} '{cmd}'")
+    return wcb.weechat.command(obuffer, cmd)
+
+
 def auto_op_timer_event(wcb, event):
-    return wcb.WEECHAT_RC_OK
+    if 'auto-op-queue' not in wcb.state:
+        return
+
+    for servername in wcb.state['auto-op-queue']:
+        for channel in wcb.state['auto-op-queue'][servername]:
+            for operation in wcb.state['auto-op-queue'][servername][channel]:
+                while True:
+                    numNicks = len(wcb.state['auto-op-queue'][servername][channel][operation])
+
+                    if numNicks == 0:
+                        break
+
+                    nicklist = ''
+                    if numNicks >= 3:
+                        nicklist = " ".join(wcb.state['auto-op-queue'][servername][channel][operation][:3])
+                        del wcb.state['auto-op-queue'][servername][channel][operation][:3]
+                    else:
+                        nicklist = " ".join(wcb.state['auto-op-queue'][servername][channel][operation])
+                        wcb.state['auto-op-queue'][servername][channel][operation] = []
+
+                    cmd = f"/{operation} {nicklist}"
+                    _find_buffer_and_do(wcb, servername, channel, cmd)
+    return wcb.weechat.WEECHAT_RC_OK
+
 
 def run(wcb, event):
     if event['signal'] == 'timer_signal':
@@ -47,8 +84,21 @@ def run(wcb, event):
             wcb.mlog(f"Nick '{tgtNick}' on channel '{event['channel']}' already has {cmd}")
             wcb.weechat.infolist_free(infolist)
             return wcb.weechat.WEECHAT_RC_OK
-
     wcb.weechat.infolist_free(infolist)
+
+    if not event['command_args']:
+        # add to queue
+        if 'auto-op-queue' not in wcb.state:
+            wcb.state['auto-op-queue'] = {}
+        if event['server'] not in wcb.state['auto-op-queue']:
+            wcb.state['auto-op-queue'][event['server']] = {}
+        if event['channel'] not in wcb.state['auto-op-queue'][event['server']]:
+            wcb.state['auto-op-queue'][event['server']][event['channel']] = {}
+        if cmd not in wcb.state['auto-op-queue'][event['server']][event['channel']]:
+            wcb.state['auto-op-queue'][event['server']][event['channel']][cmd] = []
+        if tgtNick not in wcb.state['auto-op-queue'][event['server']][event['channel']][cmd]:
+            wcb.state['auto-op-queue'][event['server']][event['channel']][cmd].append(tgtNick)
+        return wcb.weechat.WEECHAT_RC_OK
 
     log = f"Gave {cmd} to '{tgtNick}' on channel '{event['channel']}'"
     if event['command_args']:
